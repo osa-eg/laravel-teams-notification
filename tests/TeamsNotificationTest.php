205dbc0bf07615b40458b6ae5d572dcf47523c82
Osama Saad <osama.96.eg@gmail.com>
2024-07-24 21:31:05 +0300

    update test

--- tests/TeamsNotificationTest.php
+++ tests/TeamsNotificationTest.php
@@ -1,97 +1,268 @@
 <?php
 
-namespace Osama\LaravelTeamsNotification\Tests;
+namespace Tests\Unit;
 
+use Illuminate\Support\Facades\Http;
 use Osama\LaravelTeamsNotification\TeamsNotification;
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use Mockery;
 
 class TeamsNotificationTest extends TestCase
 {
-    protected $notification;
-
-    protected function setUp(): void
+    protected function setUp()
     {
         parent::setUp();
-        $this->notification = new TeamsNotification();
+
+        // Mocking HTTP requests to avoid actual API calls
+        Http::fake();
     }
 
-    public function testSendNormalMessage()
+    public function testSendMessage()
     {
-        $message = "This is a normal message.";
-        $result = $this->notification->sendNormalMessage($message);
+        $notification = new TeamsNotification();
+        $response = $notification->setColor('good')->sendMessage('Test Message', ['Key1' => 'Value1', 'Key2' => 'Value2']);
 
-        $this->assertTrue($result);
-    }
-
-    public function testSendExceptionMessage()
-    {
-        $exception = new \Exception("Test Exception", 123);
-        $result = $this->notification->sendException($exception);
-
-        $this->assertTrue($result);
-    }
-
-    public function testSendJsonBlockMessage()
-    {
-        $data = [
-            'key1' => 'value1',
-            'key2' => 'value2',
+        $expected = [
+            "type" => "message",
+            "attachments" => [
+                [
+                    "contentType" => "application/vnd.microsoft.card.adaptive",
+                    "content" => [
+                        "\$schema" => "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "type" => "AdaptiveCard",
+                        "version" => "1.2",
+                        "msteams" => [
+                            "width" => "Full"
+                        ],
+                        "body" => [
+                            [
+                                "type" => "TextBlock",
+                                "size" => "Medium",
+                                "weight" => "Bolder",
+                                "color" => "good",
+                                "text" => 'Test Message'
+                            ],
+                            [
+                                "type" => "ColumnSet",
+                                "columns" => [
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => 'Key1',
+                                                "weight" => "Bolder"
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ],
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => 'Value1',
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "type" => "ColumnSet",
+                                "columns" => [
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => 'Key2',
+                                                "weight" => "Bolder"
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ],
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => 'Value2',
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
         ];
-        $result = $this->notification->sendJsonBlock($data);
 
-        $this->assertTrue($result);
+        Http::assertSent(function ($request) use ($expected) {
+            return $request->data() === $expected;
+        });
     }
 
-    public function testSendMessageWithCustomColor()
+    public function testSendException()
     {
-        $message = "This is a message with a custom color.";
-        $result = $this->notification->setColor('accent')->sendNormalMessage($message);
+        $exception = new \Exception('Test Exception', 1, new \ErrorException('Previous exception'));
 
-        $this->assertTrue($result);
+        $notification = new TeamsNotification();
+        $notification->bindTrace(); // To include the trace
+        $response = $notification->sendException($exception);
+
+        $expected = [
+            "type" => "message",
+            "attachments" => [
+                [
+                    "contentType" => "application/vnd.microsoft.card.adaptive",
+                    "content" => [
+                        "\$schema" => "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "type" => "AdaptiveCard",
+                        "version" => "1.2",
+                        "msteams" => [
+                            "width" => "Full"
+                        ],
+                        "body" => [
+                            [
+                                "type" => "TextBlock",
+                                "size" => "Large",
+                                "weight" => "Bolder",
+                                "text" => "Exception Occurred",
+                                "color" => "attention"
+                            ],
+                            [
+                                "type" => "ColumnSet",
+                                "columns" => [
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "Message:",
+                                                "weight" => "Bolder"
+                                            ],
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => 'Test Exception',
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ],
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "File:",
+                                                "weight" => "Bolder"
+                                            ],
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => '',
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ],
+                                    [
+                                        "type" => "Column",
+                                        "items" => [
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => "Line:",
+                                                "weight" => "Bolder"
+                                            ],
+                                            [
+                                                "type" => "TextBlock",
+                                                "text" => '',
+                                                "wrap" => true
+                                            ]
+                                        ],
+                                        "width" => "stretch"
+                                    ]
+                                ]
+                            ],
+                            [
+                                "type" => "TextBlock",
+                                "text" => "Trace:",
+                                "weight" => "Bolder"
+                            ],
+                            [
+                                "type" => "TextBlock",
+                                "text" => nl2br($exception->getTraceAsString()),
+                                "wrap" => true
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        Http::assertSent(function ($request) use ($expected) {
+            return $request->data() === $expected;
+        });
     }
 
-    public function testSendSuccessMessage()
+    public function testSendJsonMessage()
     {
-        $message = "This is a success message.";
-        $result = $this->notification->success()->sendNormalMessage($message);
+        $notification = new TeamsNotification();
+        $response = $notification->sendJsonMessage('JSON Message', ['key1' => 'value1', 'key2' => 'value2']);
 
-        $this->assertTrue($result);
+        $expected = [
+            "type" => "message",
+            "attachments" => [
+                [
+                    "contentType" => "application/vnd.microsoft.card.adaptive",
+                    "content" => [
+                        "\$schema" => "http://adaptivecards.io/schemas/adaptive-card.json",
+                        "type" => "AdaptiveCard",
+                        "version" => "1.2",
+                        "msteams" => [
+                            "width" => "Full"
+                        ],
+                        "body" => [
+                            [
+                                "type" => "TextBlock",
+                                "size" => "Medium",
+                                "weight" => "Bolder",
+                                "color" => "default",
+                                "text" => 'JSON Message'
+                            ],
+                            [
+                                "type" => "TextBlock",
+                                "text" => json_encode(['key1' => 'value1', 'key2' => 'value2'], JSON_PRETTY_PRINT),
+                                "wrap" => true
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        Http::assertSent(function ($request) use ($expected) {
+            return $request->data() === $expected;
+        });
     }
 
-    public function testSendWarningMessage()
+    public function testSetColor()
     {
-        $message = "This is a warning message.";
-        $result = $this->notification->warning()->sendNormalMessage($message);
+        $notification = new TeamsNotification();
+        $notification->setColor('dark');
 
-        $this->assertTrue($result);
+        $this->assertEquals('dark', $notification->color);
     }
 
-    public function testSendErrorMessage()
+    public function testInvalidColor()
     {
-        $message = "This is an error message.";
-        $result = $this->notification->error()->sendNormalMessage($message);
+        $this->expectException(\InvalidArgumentException::class);
 
-        $this->assertTrue($result);
-    }
-
-    public function testSenderNameAndImage()
-    {
-        $this->notification->senderName("Test Sender");
-        $this->notification->senderImageUrl("https://example.com/image.jpg");
-
-        $message = "Message with sender details.";
-        $result = $this->notification->sendNormalMessage($message);
-
-        $this->assertTrue($result);
-    }
-
-    public function testBindTrace()
-    {
-        $exception = new \Exception("Test Exception with trace");
-        $this->notification->bindTrace($exception->getTrace());
-
-        $result = $this->notification->sendException($exception);
-
-        $this->assertTrue($result);
+        $notification = new TeamsNotification();
+        $notification->setColor('invalid_color');
     }
 }
